da91c5fe63da5c2a2ac2d1767ebe9fbc484ec1e0
AlshimaaGamalAlsaied <al-sh.gamal@outlook.com>
2021-03-20 01:31:33 +0300

    langing page

---
+++ landing-page/README.md
@@ -0,0 +1,20 @@
+# Landing Page Project
+## Project Brief
+This is a multi-section landing page, with a dynamically updating navigational menu based on the amount of content that is added to the page.
+
+## Table of Contents
+
+* HTML page with five sections.
+* The navigation menu.
+* Distinguish the section in view.
+* Clicking on a navigation item to scroll to sections.
+
+## Features
+1- This is an interactive page built using HTML, CSS styling, and JavaScript. The purpose of using the JavaScript file is to convert this project from a static project to a dynamic one.
+
+2- There is a navigation menu, which allows the user to scroll to the appropriate section.
+
+3- During scrolling this page, the current section will be colored.
+
+4- There is also a `Top Button`, which allows the user to scroll to the top of the page when scrolling below the fold of the page.
+
---
+++ landing-page/js/app.js
@@ -0,0 +1,94 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+const sections = document.querySelectorAll(".landing__container"); // To get all sections to add dynamic navigation to each of them
+const uls = document.querySelector("#navbar__list"); //get the empty ul to append new section links 
+const mybutton = document.getElementById("topButton"); // for scroll to top button
+const frag = document.createDocumentFragment(); // for high performance
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+
+function append(parent, child) {
+    return parent.append(child);
+};
+// to remove bounderies form unactive section
+function remove(element){
+    return element.style.background = "";
+};
+
+// set active section with "purple" bounderies, and call remove function to remove this color from any else section
+function active() {
+        for (section of sections) {
+            // get bounderies vales
+            const bounding = section.getBoundingClientRect();
+            if (bounding.top >= -50 && bounding.top <= 100) {
+                // to remove unactive section during scrolling
+                sections.forEach(remove);
+                section.style.background = "purple";
+            }
+        }
+};   
+/** functions to handle scroll to top*/
+function scrollFunction() {
+    if (document.body.scrollTop > 2800 || document.documentElement.scrollTop > 2800) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+};
+  
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+};
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+// create menu with unordered list linked to each section
+sections.forEach(function (section, index) {
+    const dataNav = "{Section " + (index + 1) + "} ";
+    const textNode = document.createTextNode(dataNav);
+    const li_sec = document.createElement("li");
+    const link_sec = document.createElement("a");
+    link_sec.addEventListener('click', function () {
+        section.scrollIntoView({ behavior: "smooth" });
+    });
+    append(link_sec, textNode);
+    append(li_sec, link_sec);
+    append(frag, li_sec);
+});
+
+append(uls, frag);
+
+//scroll event
+window.addEventListener('scroll', active);
+
+
+// to top button
+window.onscroll = scrollFunction;
+
+
